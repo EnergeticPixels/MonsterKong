e5dffe70274bb629abd8e17544c6be949e55d297
Anthony Jackman <energeticpixels@gmail.com>
2019-04-18 15:20:42 -0500

    adds player animation during movement

--- scripts/game.js
+++ scripts/game.js
@@ -73,8 +73,16 @@ var GameState = {
     this.player.body.velocity.x = 0;
     if(this.cursors.left.isDown || this.player.customParams.isMovingLeft) {
       this.player.body.velocity.x = -this.RUNNING_SPEED;
+      this.player.scale.setTo(1, 1);
+      this.player.play('walking');
     }  else if(this.cursors.right.isDown || this.player.customParams.isMovingRight) {
       this.player.body.velocity.x = this.RUNNING_SPEED;
+      // flips the player to face the other way
+      this.player.scale.setTo(-1, 1);
+      this.player.play('walking');
+    } else {
+      this.player.animations.stop();
+      this.player.frame = 3;
     };
     if((this.cursors.up.isDown || this.player.customParams.mustJump) && this.player.body.touching.down) {
       this.player.body.velocity.y = -this.JUMPING_SPEED;
